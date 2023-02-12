d9d0d779f1a088dcd552cd5993c4e57ecebf1599
behnam1369 <behnam.aghaali@yahoo.com>
2023-02-12 19:59:12 +0330

    Create translation migration file

---
+++ db/migrate/20230101161811_create_translations.rb
@@ -0,0 +1,26 @@
+class CreateTranslations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :translations do |t|
+      t.string :english
+      t.string :persian
+      t.string :arabic
+      t.string :spanish
+      t.string :french
+      t.string :turkish
+      t.string :portugess
+      t.string :dutch
+      t.string :russian
+      t.string :italian
+      t.string :swedish
+      t.string :hindi
+      t.string :urdu
+      t.string :chiness
+      t.string :japaness
+      t.string :korean
+      t.string :thai
+      t.string :malay
+      t.string :indonesian
+      t.timestamps
+    end
+  end
+end
